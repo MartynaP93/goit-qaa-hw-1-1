f3302265b8f7e03fb89ae09529fc0d0def36d506
MartynaP93 <martyna.anna.piotrowska@gmail.com>
2024-07-10 12:03:08 +0200

    modul7

--- js/task-02.js
+++ js/task-02.js
@@ -13,6 +13,6 @@ ingredients.forEach((ingredient) => {
   const li = document.createElement("li");
   li.textContent = ingredient;
   li.classList.add("item");
-  ulIngredients.append(li);
   console.log(li);
+  ulIngredients.append(li);
 });
